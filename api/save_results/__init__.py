6532fe75957c6eae353509a6349b41abacf5f650
sachak <sacha.karajovic@gmail.com>
2025-07-11 17:56:54 +0200

    Update __init__.py

--- api/save_results/__init__.py
+++ api/save_results/__init__.py
@@ -10,7 +10,7 @@ except Exception as e:
             status_code=200,
             mimetype="application/json"
         )
-import os, io, json, logging, traceback
+
 
 import os, io, json, logging, traceback
 import pandas as pd
